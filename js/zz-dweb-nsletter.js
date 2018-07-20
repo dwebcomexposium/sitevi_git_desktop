e5c3e6093436fe6e0f5b20f4b66f3288ea48ddfd
ThomasRny <33687666+ThomasRny@users.noreply.github.com>
2018-07-20 15:13:45 +0200

    Initialized SITEVI Desktop from kloudspeaker

---
+++ js/zz-dweb-nsletter.js
@@ -0,0 +1,7 @@
+/* Newsletter fix */
+
+jQuery(function($){
+
+$('#zone1 > div.block.block-small.quicklinks.side-navigation > ul > li.ql-item.linkid175902 > a').attr("onclick","xt_med('C','','::Newsletter_subscription','N')");
+
+});
